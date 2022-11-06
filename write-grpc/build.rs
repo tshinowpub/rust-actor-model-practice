9b4d4278d706ecf34d962d37d4768139e4268f82
tshinowpub <tshinow@gmail.com>
2022-11-06 22:49:57 +0900

    Add message grpc service.

--- write-grpc/build.rs
+++ write-grpc/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    tonic_build::compile_protos("proto/message.proto").unwrap();
+    tonic_build::compile_protos("proto/channel/channel.proto").unwrap();
+    tonic_build::compile_protos("proto/message/message.proto").unwrap();
 }
--- write-grpc/src/main.rs
+++ write-grpc/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use crate::message::message_server::{Message, MessageServer};
+use message::{MessageReply, MessageRequest};
+
+pub mod message {
+    tonic::include_proto!("message");
+}
+
+#[derive(Default)]
+pub struct MyMessage {}
+
+#[tonic::async_trait]
+impl Message for MyMessage {
+    async fn add_message(
+        &self,
+        request: Request<MessageRequest>,
+    ) -> Result<Response<MessageReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = message::MessageReply {
+            message: format!("Hello {}!", request.into_inner().value),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let message = MyMessage::default();
+
+    println!("MessageServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(MessageServer::new(message))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
