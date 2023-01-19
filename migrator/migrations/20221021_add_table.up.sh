47d4356e5c418ec85d4c926d3b8912c67741fbf0
tshinowpub <tshinow@gmail.com>
2023-01-20 04:30:41 +0900

    Add migration script.

---
+++ migrator/migrations/20221021_add_table.up.sh
@@ -0,0 +1,12 @@
+aws dynamodb create-table \
+    --endpoint http://127.0.0.1:8000 \
+    --table-name Music \
+    --attribute-definitions \
+        AttributeName=Artist,AttributeType=S \
+        AttributeName=SongTitle,AttributeType=S \
+    --key-schema \
+        AttributeName=Artist,KeyType=HASH \
+        AttributeName=SongTitle,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --table-class STANDARD
