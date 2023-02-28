4efd8f11b97a64d37b7d05f3e3fc72a5de758ef0
tshinowpub <tshinow@gmail.com>
2023-03-01 07:58:43 +0900

    Add worker & consumer frame .

---
+++ read-model-updater/src/consumer.rs
@@ -0,0 +1,22 @@
+use anyhow::{anyhow, Result};
+
+#[derive(Debug, Default)]
+pub struct Consumer {
+}
+
+impl Consumer {
+    pub fn consume(self) -> Result<()> {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::consumer::Consumer;
+
+    #[test]
+    fn test_consumer() -> anyhow::Result<()> {
+        let consumer = Consumer::default();
+
+        consumer.consume()
+    }
+}
--- read-model-updater/src/main.rs
+++ read-model-updater/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub mod consumer;
+
+/*
+ * @see https://docs.rs/signal-hook/latest/signal_hook/
+ * @see https://dev.to/talzvon/handling-unix-kill-signals-in-rust-55g6
+ * @see https://qiita.com/qnighy/items/4bbbb20e71cf4ae527b9
+ */
+fn main() -> Result<()> {
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
+
+    while !term.load(Ordering::Relaxed) {
+        println!("Hello, world!");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_main() {
+    }
 }
