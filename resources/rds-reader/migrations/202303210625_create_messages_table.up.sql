a75c9539d68dccf03f5260f022b71ac8d1b89813
tshinowpub <tshinow@gmail.com>
2023-03-23 06:33:10 +0900

    Fix migration message table.

--- resources/rds-reader/migrations/202303210625_create_messages_table.up.sql
+++ resources/rds-reader/migrations/202303210625_create_messages_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS messages (
     message_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    message_write_id VARCHAR(32) NOT NULL,
+    message_write_id VARCHAR(36) NOT NULL,
     account_id int(10) UNSIGNED NOT NULL,
     channel_id int(10) UNSIGNED NOT NULL,
     message text NOT NULL,
